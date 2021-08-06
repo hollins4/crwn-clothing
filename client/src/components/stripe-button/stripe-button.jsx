64f71655d69d79a748a61941a079cae863625777
hollins4 <graham.hollingsworth@outlook.com>
2021-08-05 22:21:41 -0400

    separating our crown app into client folder to configure backend for stripe

--- client/src/components/stripe-button/stripe-button.jsx
+++ client/src/components/stripe-button/stripe-button.jsx
@@ -1,13 +1,28 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
+
 
 const StripeCheckoutButton = ({ price }) => {
     const priceForStripe = price * 100;
     const publishableKey = 'pk_test_51JFAJfFxd0pIQOCWXeBfM4AfpV0kbWilnXvkuYWseEkRIisxjCOhMmeL4ncEiql9XEqzBYEO9QHAQmgtwWKR0BR200ZZC2R1hj';
 
     const onToken = token => {
-        console.log(token);
-        alert("Payment Successfil")
+        axios({
+            url: 'payment',
+            method: 'post',
+            data: {
+                amount: priceForStripe,
+                token
+            }
+        }).then(response => {
+            alert('Pyament successful')
+        }).catch(error => {
+            console.log('Payment error: ', JSON.parse(error));
+            alert(
+                'There was an issue with your payment. Please make sure you use the provided credit card'
+            )
+        })
     }
 
     return (
