2af9718c489204020e827e4f8cdc04770fd9e117
hollins4 <graham.hollingsworth@outlook.com>
2021-08-15 07:30:47 -0400

    updating our code to be pwa complaint

--- client/src/index.js
+++ client/src/index.js
@@ -3,8 +3,9 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react'; 
-import { store, persistor } from './redux/store';
 
+import { store, persistor } from './redux/store';
+import * as serviceWorker from './service-worker';
 import './index.css';
 import App from './App';
 
@@ -22,5 +23,7 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root')
 );
+
+serviceWorker.register();
 
 
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const path = require('path')
+const enforce = require('express-sslify');
 const compression = require('compression')
 
 
@@ -17,6 +18,7 @@ app.use(bodyParser.urlencoded({ extended: true }))
 
 
 if (process.env.NODE_ENV === 'production') {
+    app.use(enforce.HTTPS({ trustProtoHeader: true }));
     app.use(express.static(path.join(__dirname, 'client/build')));
 
     app.get('*', function(req, res)  {
@@ -28,6 +30,10 @@ app.listen(port, error => {
     if (error) throw error
     console.log('Server running on port' + port)
 })
+
+app.get('/service-worker.js', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '..', 'build', 'service-worker.js'));
+});
 
 app.post('/payment', (req, res) => {
     const body = {
